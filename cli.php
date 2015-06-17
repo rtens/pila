c61dfc7f62afd93ca986b5476becbd33e2b1a8d4
Nikolas Martens <nikolas.martens@researchgate.net>
2015-06-17 10:39:01 +0200

    Added CLI script

---
+++ cli.php
@@ -0,0 +1,66 @@
+<?php
+
+include __DIR__ . "/vendor/autoload.php";
+
+$results = (new \org\rtens\isolation\Runner(__DIR__ . '/src/libraries'))->run();
+
+if (!$results) {
+    echo "No results" . PHP_EOL;
+    exit();
+}
+
+$accessors = [
+    'Library' => 'getLibraryName',
+    'Quality' => 'getQuality',
+    'Points' => 'getPoints',
+    'Message' => 'getMessage'
+];
+
+$width = [
+    'Library' => 12,
+    'Quality' => 20,
+    'Points' => 6,
+    'Message' => 60
+];
+
+$scores = [];
+
+foreach ($results as $result) {
+    if (!array_key_exists($result->getLibraryName(), $scores)) {
+        $scores[$result->getLibraryName()] = 0;
+    }
+    $scores[$result->getLibraryName()] += $result->getPoints();
+}
+krsort($scores);
+
+print(PHP_EOL);
+print('High score list' . PHP_EOL);
+print('~~~~~~~~~~~~~~~' . PHP_EOL . PHP_EOL);
+
+foreach ($scores as $library => $score) {
+    printf('%-12s %2d' . PHP_EOL, $library, $score);
+}
+
+print(PHP_EOL);
+print(PHP_EOL);
+print('Detailed assessment' . PHP_EOL);
+print('~~~~~~~~~~~~~~~~~~~' . PHP_EOL . PHP_EOL);
+
+foreach ($accessors as $library => $accessor) {
+    printf('%-' . $width[$library] . 's | ', $library);
+}
+
+print(PHP_EOL);
+
+foreach ($accessors as $library => $accessor) {
+    print(str_repeat('-', $width[$library]) . '-+-');
+}
+
+foreach ($results as $result) {
+    print(PHP_EOL);
+    foreach ($accessors as $library => $accessor) {
+        printf("%-" . $width[$library] . "s | ", $result->$accessor());
+    }
+}
+
+print(PHP_EOL);

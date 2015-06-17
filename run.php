856d388c51e5461d0425bb1f253e3bc1aa066c09
Nikolas Martens <nikolas.martens@researchgate.net>
2015-06-17 10:15:09 +0200

    Remove temporary run script

--- run.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use org\rtens\isolation\Runner;
-
-require_once __DIR__ . '/vendor/autoload.php';
-
-var_dump((new Runner(__DIR__ . '/src/libraries', \org\rtens\isolation\Library::class))->run());
